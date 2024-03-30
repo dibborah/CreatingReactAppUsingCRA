cb80fa189d5321fdebba0565d586e15b67d7ebd6
dibya.borah <dibyajyotiborah469@gmail.com>
2024-03-30 19:23:08 +0530

    JSX Rules ch 1 unit 10

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import HelloWorld from "./components/HelloWorld";
 import Greetings from "./components/Greetings";
 
 function App() {
   return (
     <div className="App">
-      <HelloWorld />
       <Greetings/>
     </div>
   );
--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -1,7 +1,35 @@
+import "./Greetings.css";
+
 function Greetings() {
+    const firstName = "Mark";
+
+  //   const firstName = (name) => {
+  //       return name.toUpperCase();
+  //   }
+
+  // function firstName(name) {
+  //   return name.toUpperCase();
+  // }
+
+  // This below is a function expression
+  // const firstName = function(name) {
+  //   return name.toUpperCase();
+  // }
+
+//   const firstName = (name) => name.toUpperCase();
+
   return (
-    <div>Greetings</div>
-  )
+    <div className="bg-dark">
+      <h1>Hello, {`${firstName} how is META going on !!!`}</h1>
+      <h1>{ 2 + 3}</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque,
+        expedita?
+      </p>
+    </div>
+  );
 }
+
+// # Note: JSX ke andaar agar aapko JavaScript ki duniya main aana hain to aapko {} "curly braces lagane h parengge !!!"
 
 export default Greetings;
